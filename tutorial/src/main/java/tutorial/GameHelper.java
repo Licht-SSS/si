832a39bcc04a280c689d3e63f2f497efa3e12e3f
onesword0618 <ao.akua.leo@gmail.com>
2019-07-07 23:46:50 +0900

    Add: 研修用資料追加

---
+++ tutorial/src/main/java/tutorial/GameHelper.java
@@ -0,0 +1,94 @@
+package tutorial;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class GameHelper {
+	private static final String alpabet = "abcdefg";
+	private int gridLength = 7;
+	private int gridSize = 49;
+	private int[] grid = new int[gridSize];
+	private int comCount = 0;
+
+	/**
+	 * 入力値の検査を行う
+	 * 
+	 * @param prompt　入力値
+	 * @return 妥当かどうか
+	 */
+	public String getUserInput(String prompt) {
+		String inputLine = null;
+		System.out.print(prompt + "");
+		
+		try {
+			BufferedReader is = new BufferedReader(new InputStreamReader(System.in));
+			inputLine = is.readLine();
+
+			if (inputLine.length() == 0) {
+				return null;
+			}
+			
+		} catch (IOException e) {
+			System.out.println("IOException:" + e);
+		}
+		return inputLine.toLowerCase();
+	}
+
+	/**
+	 * ゲーム開始時の軍艦を設定する。
+	 * 
+	 * @param comSize 軍艦のサイズ
+	 * @return
+	 */
+	public ArrayList<String> placeShipGame(int comSize) {
+
+		ArrayList<String> alphaCells = new ArrayList<>();
+		String temp = null;
+		int[] coords = new int[comSize];
+		int attempts = 0;
+		boolean success = false;
+		int location = 0;
+		comCount++;
+		int incr = 1;
+		
+		if ((comCount % 2) == 1) {
+			incr = gridLength;
+		}
+
+		while (!success & attempts++ < 200) {
+			location = (int) (Math.random() * gridSize);
+			int x = 0;
+			success = true;
+			while (success && x < comSize) {
+				if (grid[location] == 0) {
+					coords[x++] = location;
+					location += incr;
+					if (location >= gridSize) {
+						success = false;
+					}
+					if (x > 0 && (location % gridLength == 0)) {
+						success = false;
+					}
+				} else {
+					success = false;
+				}
+			}
+		}
+
+		int x = 0;
+		int row = 0;
+		int column = 0;
+
+		while (x < comSize) {
+			grid[coords[x]] = 1;
+			row = (int) (coords[x] / gridLength);
+			column = coords[x] % gridLength;
+			temp = String.valueOf(alpabet.charAt(column));
+			alphaCells.add(temp.concat(Integer.toString(row)));
+			x++;
+		}
+		return alphaCells;
+	}
+}
